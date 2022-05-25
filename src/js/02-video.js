d02e92e8e982962dfb36c1e041d3ab64f65b2835
unknown <andrijgrineveckij@gmail.com>
2022-05-25 15:44:39 +0300

    task 2 without throttle feature

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import Player from '@vimeo/player';
 
+const LOCAL_STORAGE_KEY = 'videoplayer-current-time';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.setCurrentTime(getCurrentTime());
+
+player.on('timeupdate', onPlayerCurrentTimeSave, 1000);
+
+function onPlayerCurrentTimeSave(event) {
+  let timeToSave = event.seconds;
+  localStorage.setItem(LOCAL_STORAGE_KEY, timeToSave);
+  console.log('timeupdate', event);
+}
+
+function getCurrentTime() {
+  return localStorage.getItem(LOCAL_STORAGE_KEY);
+}
